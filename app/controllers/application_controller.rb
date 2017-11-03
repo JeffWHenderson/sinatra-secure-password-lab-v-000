ad47159b4a9622a0fc0eabe2f3672c798416a35a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 17:35:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    User.find_by(params[:username])
+    User.find_by(:username => params[:username])
   end
 
   get '/account' do
