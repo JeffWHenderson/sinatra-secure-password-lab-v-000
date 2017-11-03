ddbf989986cec9d633400d2554f5cf3683d6b2fc
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 18:04:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     #your code here
     user = User.new(:username => params[:username], :password => params[:password])
 
-    if user.save && user.username.size > 0
+    if user.save && user.username.size > 0  #leaving a blank username was giving me and empty string, which is truthy.  so I had to make sure the username sting is greater than one and that it saves
       redirect "/login"
     else
       redirect "/failure"
